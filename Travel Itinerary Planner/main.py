f3354be8921fdb6080ac80118bcf85787d3d26f0
20111596 <20111596@tafe.wa.edu.au>
2025-11-27 20:42:17 +0800

    Feat: Added add_itinerary functionality to main program (main.py) and function handler (manage_itineraries.py). Also added requirements.txt for pip install.

--- Travel Itinerary Planner/main.py
+++ Travel Itinerary Planner/main.py
@@ -10,48 +10,115 @@ def user_task_request(request, itinerary_list):
     if request == "1":
         flights_list_done = False
         attractions_list_done = False
+
         print("How exciting! Please provide us information about the trip: \n")
         name = input("Title of the itinerary: ")
         location = input("Location (NA if not applicable): ")
         summary = input("Brief description of trip: ")
         start_date = input("Start date in DD-MM-YYYY: ")
         end_date = input("End date in DD-MM-YYYY: ")
-        while not flights_list_done:
-            # Answer format: Perth-Sydney 12-12-2012, Sydney-Perth 21-12-2012
-            flights = input("")  # dictionary - add loop here
-            if flights == "DONE":
-                flights_list_done = True
-        while not attractions_list_done:
-            attractions = input("")  # dictionary - add loop here
-            if attractions == "DONE":
-                attractions_list_done = True
+        flights = {}
+        attractions = {}
+
+        print("\nNow it is time to add flights!")
+        user_flight_choice = input("If you want to skip this step, type SKIP and press 'Enter'. Otherwise, press 'Enter'. ")
+        flights_list_done = False
+
+        if user_flight_choice == "SKIP":
+            flights = {}
+        else:
+            while not flights_list_done:
+                flight_details = {}
+
+                departure_airport = input("Name of the airport you will depart from: ")
+                departure_date = input("Date & time of flight departure (Format: DD-MM-YYYY HH:MM): ")
+                arrival_airport = input("Name of the airport you will arrive at: ")
+                arrival_date = input("Date & time of flight arrival (Format: DD-MM-YYYY HH:MM): ")
+                flight_name = f"{departure_airport} to {arrival_airport}"
+
+                flight_details.update({
+                    "departure airport": departure_airport,
+                    "departure date": departure_date,
+                    "arrival airport": arrival_airport,
+                    "arrival date": arrival_date
+                    })
+                flights[flight_name] = flight_details
+
+                add_another_flight = input("Would you like to add another flight? Type Y (yes) or N (no): ")
+                while True:
+                    if add_another_flight == "Y":
+                        flights_list_done = True
+                        break
+                    elif add_another_flight == "N":
+                        flights_list_done = False
+                        break
+                    else:
+                        print("Invalid answer: Please type Y or N only.")
+
+        print("\nFinally: ATTRACTIONS!")
+        user_attractions_choice = input("If you want to skip this step, type SKIP and press 'Enter'. Otherwise, press 'Enter'. ")
+        attractions_list_done = False
+
+        if user_attractions_choice == "SKIP":
+            attractions = {}
+        else:
+            while not attractions_list_done:
+                attraction_details = {}
+
+                attraction_name = input("Name of attraction: ")
+                attraction_address = input("Address of attraction: ")
+                attraction_summary = input("Short description of attraction: ")
+                attraction_type = input("(Optional) Provide some tags that categorise what kind of activity this involves.\nExample of format required: hike, exciting, views\n")
+                attraction_tags = attraction_type.split(", ")
+
+                attraction_details.update({
+                    "address": attraction_address,
+                    "summary": attraction_summary,
+                    "tag(s)": attraction_tags
+                    })
+                attractions[attraction_name] = attraction_details
+
+                add_another_attraction = input("Would you like to add another attraction? Type Y or N:")
+                while True:
+                    if add_another_attraction == "Y":
+                        attractions_list_done = True
+                        break
+                    elif add_another_attraction == "N":
+                        attractions_list_done = False
+                        break
+                    else:
+                        print("Invalid answer: Please type Y or N only.")
+
         add_itinerary(itinerary_list, name, location, summary, start_date, end_date, flights, attractions)
-        pass
 
     # Edit existing itinerary
     elif request == "2":
         # Use pickpack module (https://github.com/anafvana/pickpack#map-function-for-nested-lists)
+        chosen_itinerary = input("")
+        edit_itinerary(itinerary_list, chosen_itinerary)
         pass
-    
+
     # View itinerary
     elif request == "3":
-        view_itineraries(itinerary_list)
+        chosen_itinerary = input("")
+        view_itineraries(itinerary_list, chosen_itinerary)
         pass
 
     # Delete itinerary
     elif request == "4":
-        delete_itinerary(itinerary_list)
+        chosen_itinerary = input("")
+        delete_itinerary(itinerary_list, chosen_itinerary)
         pass
 
+    # Export itinerary
     elif request == "5":
-        # Export to .csv file?
-        # Use pick (not pickpack) library to list itineraries by name and location
-        export_itinerary(itinerary_list)
+        chosen_itinerary = input("")
+        export_itinerary(itinerary_list, chosen_itinerary)
         pass
 
     else:
         print("Invalid answer, please type a number between 1-5.")
-        pass
+        return
 
 
 def run_app():
---
+++ Travel Itinerary Planner/src/manage_itineraries.py
@@ -0,0 +1,98 @@
+from src.itinerary import Itinerary
+from datetime import datetime
+
+"""
+This file contains all of the functions needed for the Travel Itinerary Planner.
+It allows the user to add, edit, view, delete, and export an itinerary while checking for errors in input.
+"""
+
+
+def add_itinerary(itinerary_list, name, location, summary, start_date, end_date, flights, attractions):
+    """
+    Add a new itinerary to the list of itineraries.
+
+    Args:
+        itinerary_list (list): The list of existing Itinerary objects.
+        location (str): The main city/country the holiday takes place.
+        summary (str, optional): A brief summary of the travel plan.
+        start_date (str): The date the holiday begins in 'DD-MM-YYYY' format.
+        end_date (str): The date the holiday ends in 'DD-MM-YYYY' format.
+        flights (nested dict): A nested dictionary type. Flight name (before-after location format, e.g. Perth-Sydney) is tied to a date in 'DD-MM-YYYY' format.
+        attractions (nested dict): Dictionary of attractions. Each dictionary key (name of attraction) contains a short description of the attraction (object).
+
+    Returns:
+        bool: True if Itinerary is added without issue, otherwise False.
+
+    Raises:
+        ValueError: If the start or end date is not in the correct format.
+
+    Side Effects:
+        - Saves the updated itinerary list to a file using `update_itinerary`.
+    """
+    # Prevent duplicate itineraries
+    if any(trip.name == name for trip in itinerary_list):
+        print("Error: A trip with this name already exists!")
+        return False
+
+    if not validate_dates(start_date, end_date, flights):
+        return False
+
+    # Add the new itinerary after validation checks
+    itinerary_list.append(Itinerary(name, location, summary, start_date, end_date, flights, attractions))
+
+
+def edit_itinerary(itinerary_list, chosen_itinerary):
+    # Use pickpack module (https://github.com/anafvana/pickpack#map-function-for-nested-lists)
+    pass
+
+
+def view_itineraries(itinerary_list, chosen_itinerary):
+    if not itinerary_list:
+        print("No itineraries planned!")
+    else:
+        # Use pick module: Ask if they would like to view all itineraries, or a specific one
+        pass
+    return
+
+
+def delete_itinerary(itinerary_list, chosen_itinerary):
+    print("What would you like to delete?")
+    # Use pickpack module (https://github.com/anafvana/pickpack#map-function-for-nested-lists)
+    pass
+
+
+def export_itinerary(itinerary, chosen_itinerary):
+    # Export to .pdf or .csv file
+    # Use pick (not pickpack) library to list itineraries by name and location
+    pass
+
+
+# Validation functions
+
+def validate_dates(start_date, end_date, flights):
+    # Validate dates given for start date, end date and flight datetimes
+    try:
+        datetime.strptime(start_date, "%d-%m-%Y")
+    except ValueError:
+        print("Error: Invalid start date. Use 'DD-MM-YYYY' format.")
+        return False
+
+    try:
+        datetime.strptime(end_date, "%d-%m-%Y")
+    except ValueError:
+        print("Error: Invalid end date. Use 'DD-MM-YYYY' format.")
+        return False
+
+    # Resource used for following code: https://stackoverflow.com/questions/17322208/multiple-try-codes-in-one-block
+    for flight_name, flight_info in flights.items():
+        try:
+            datetime.strptime(flight_info["departure_time"], "%d-%m-%Y %H:%M")
+        except ValueError:
+            print("Error: Invalid date/time for flight departure. Use 'DD-MM-YYYY HH:MM' format.")
+            return False
+
+        try:
+            datetime.strptime(flight_info["arrival_time"], "%d-%m-%Y %H:%M")
+        except ValueError:
+            print("Error: Invalid date/time for flight arrival. Use 'DD-MM-YYYY HH:MM' format.")
+            return False
